7153595185cdb08efaf254e23d63a315dd9ba075
zaru <sakuraba@basicinc.jp>
2015-10-14 10:00:38 +0900

    first commit

---
+++ lib/itamae/plugin/recipe/php56.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/php56/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Php56
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/php56/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Php56
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
